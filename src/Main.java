d8cc38fca1977adcf8d8440abf15e922fe6cd1b3
JosephReed75 <josephreed13@yahoo.com>
2023-02-27 18:26:06 -0500

    Started refactoring Roman Numeral Converter

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
 
     // Main Method
     public static void main(String[] args) {
-
-        RomanNumeralConverter intToRoman = new RomanNumeralConverter(2022);
-        System.out.println(intToRoman);
+        new RomanNumeralConverter(2022);
     }
 }
